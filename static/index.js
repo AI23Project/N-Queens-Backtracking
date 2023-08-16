6bb066832ffa1d8061a3f3f6ccdadf9219970f07
AMRRAGHEB118 <amrragheb118@gmail.com>
2023-08-16 09:52:37 +0300

    display result table

--- static/index.js
+++ static/index.js
@@ -1,13 +1,27 @@
 const n_queens_input = document.getElementById('n_queens_input')
 const population_size = document.getElementById('population_size')
 const max_generations = document.getElementById('max_generations')
+const result_table = document.getElementById("queens_table");
 const solved_way_options = document.querySelectorAll(".solved_way_option");
 const btn = document.getElementById('btn')
 const err_message = document.getElementById('error_message')
-const result_div = document.getElementById("result")
 let result= [];
 let solved_way = "backtracking";
 
+const display_queens = (result) => {
+  const matrix = [];
+  for (let i = 0; i < result.length; i++) {
+    matrix[i] = [];
+    for (let j = 0; j < result.length; j++) {
+      if (result[i] === j) {
+        matrix[i].push("Q");
+      } else {
+        matrix[i].push(0);
+      }
+    }
+  }
+  return matrix;
+};
 
 function get_selceted_solved_way() {
   solved_way_options.forEach((option) => {
@@ -24,6 +38,7 @@ solved_way_options.forEach((option) => {
     option.classList.add("active");
   });
 });
+
 btn.addEventListener("click", (e) => {
   let url;
     e.preventDefault();
@@ -32,18 +47,24 @@ btn.addEventListener("click", (e) => {
     }
     else {
         const click_btn = async () => {
-          console.log(solved_way);
             const res = await fetch(`${url = solved_way === "backtracking" ? `/?n_queens_input=${Math.round(n_queens_input.value)}&solved_by=backtracking` : `/?n_queens_input=${Math.round(n_queens_input.value)}&solved_by=differential_evolution&population_size=${population_size.value}&max_generations=${max_generations.value}`}`, {
               method: "POST",
             });
             const data = await res.json();
             result = data.result
-            console.log(result);
-            result_div.innerHTML = ""
-            result_div.innerHTML += result
+            const matrix = display_queens(result);
+            result_table.innerHTML = ""
+            for (let i = 0; i < matrix.length; i++) {
+              const row = document.createElement("tr");
+              for (let j = 0; j < matrix[i].length; j++) {
+                const cell = document.createElement("td");
+                cell.innerHTML = matrix[i][j];
+                row.appendChild(cell);
+              }
+              result_table.appendChild(row);
+            }
           };
           click_btn()
         err_message.style.display = "none"
     }
   });
-  // 
